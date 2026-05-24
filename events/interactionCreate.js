0a6be67b54a67d1e454b9bf008c46f42e0e3809e
andrezinsamp13-boop <andrezin.samp13@gmail.com>
2026-05-23 20:00:46 -0400

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,1 +1,38 @@
+'use strict';
 
+const commands = {
+  criarticket: require('../commands/criarticket'),
+  configurarticket: require('../commands/configurarticket'),
+  mensagem: require('../commands/mensagem'),
+};
+
+module.exports = async (interaction, client) => {
+  try {
+
+    if (interaction.isChatInputCommand()) {
+
+      const cmd = commands[interaction.commandName];
+
+      if (!cmd) {
+        return interaction.reply({
+          content: '❌ Comando não encontrado.',
+          ephemeral: true,
+        });
+      }
+
+      await cmd.execute(interaction, client);
+    }
+
+  } catch (err) {
+
+    console.error('[INTERACTION ERROR]', err);
+
+    if (!interaction.replied) {
+      interaction.reply({
+        content: '❌ Ocorreu um erro ao executar este comando.',
+        ephemeral: true,
+      }).catch(() => {});
+    }
+
+  }
+};
